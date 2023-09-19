c801bea33f4ad98c7cd6524d9d8c72a9cb4af263
Sheikh Junaid <88541284+sheikhjunaid07@users.noreply.github.com>
2023-09-19 04:04:46 +0000

    Variables done

---
+++ 01_basics/Variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345;
+let accountEmail = "junaid@gmail.com"
+var accountPassword = "797423"
+accountCity = "Indore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
