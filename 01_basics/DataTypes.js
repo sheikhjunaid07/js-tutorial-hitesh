9d9fa6179fd3c4faa6986e693c2c79a40f38b4aa
Sheikh Junaid <88541284+sheikhjunaid07@users.noreply.github.com>
2023-09-23 03:53:25 +0000

    Datatypes

--- 01_basics/DataTypes.js
+++ 01_basics/DataTypes.js
@@ -1,13 +1,24 @@
 "use Strict";   //treat all JS code in Newer version 
 
 /*
-JavaScript has 8 Datatypes
-String.
-Number.
-Bigint.
-Boolean.
+JavaScript Datatypes
+String.  
+Number.      range: 2^53
+Boolean.     true/false
 Undefined.
-Null.
-Symbol.
+Null.        standalone value (empty value)
+Symbol.      for uniqueness
 Object
 */
+
+let name = "junaid"
+let num = 123
+let isVarified = true
+
+console.log(typeof "junaid");   //string
+
+console.log(typeof num)         //Number
+ 
+console.log(typeof null);       //null's type object
+ 
+console.log(typeof undefined)   //undefined   
