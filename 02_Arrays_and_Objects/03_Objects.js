bc7f4d6a38ecbd25253b8f589ea8673ab1fc1409
Sheikh Junaid <88541284+sheikhjunaid07@users.noreply.github.com>
2023-09-28 15:31:34 +0000

    about object

---
+++ 02_Arrays_and_Objects/03_Objects.js
@@ -0,0 +1,22 @@
+// object literal
+
+const mySymbol = Symbol() //symbol datatype
+
+const userDetails = {
+    name: "Junaid",
+    age: 23,
+    [mySymbol]: "key1",   //use symbol datatype in object
+    location: "Indore",
+    email: "junaid@gmail.com",
+    isLoggedIn: true
+}
+console.log(userDetails);
+//access data
+console.log(userDetails.name);  //Junaid
+//another way to access data
+console.log(userDetails["name"]);  //Junaid
+
+//freeze method - if you use freeze method you can't change the properties of object
+Object.freeze(userDetails);
+userDetails.age = 26
+console.log(userDetails);  //if you check the output age is still 23  
