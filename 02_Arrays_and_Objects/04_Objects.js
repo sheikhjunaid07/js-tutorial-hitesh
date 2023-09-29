3b03d9b16afa7c24fd27e0b0a627754d6a102004
Sheikh Junaid <88541284+sheikhjunaid07@users.noreply.github.com>
2023-09-29 05:05:19 +0000

    More About Objects

--- 02_Arrays_and_Objects/04_Objects.js
+++ 02_Arrays_and_Objects/04_Objects.js
@@ -1,27 +1,44 @@
 //other way to create object (singleton object)
 
-const userData = new Object()
+const userData = new Object();
 
-userData.name = "Parvez"
-userData.age = 26
-userData.email = 'parvez@gmail.com'
-isLoggedIn = true
+userData.name = "Parvez";
+userData.age = 26;
+userData.email = "parvez@gmail.com";
+isLoggedIn = true;
 
 console.log(userData);
 
 //declare object inside an objects
 const newUser = {
-    name: {
-        fullName: {
-            firstName: "Raj",
-            lastName: "Sharma"
-        }
+  name: {
+    fullName: {
+      firstName: "Raj",
+      lastName: "Sharma",
     },
-    age: 20,
-    email: "abc@gmail.com"
-}
+  },
+  age: 20,
+  email: "abc@gmail.com",
+};
 
 //access Data
-console.log(newUsername); 
-console.log(newUser.name.fullName.firstName);
-console.log(newUser.name.fullName.lastName);
+console.log(newUser.name); //{ name: 'Parvez', age: 26, email: 'parvez@gmail.com' }
+console.log(newUser.name.fullName.firstName); //Raj
+console.log(newUser.name.fullName.lastName); //Sharma
+
+//merge two objects
+const obj1 = {1: "a", 2: "b"} 
+const obj2 = {3: "c", 4: "d"} 
+
+const obj3 = Object.assign({}, obj1, obj2) //{} are optional they are only verify that the expected output must be an object
+console.log(obj3);  //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+//another way to merge data
+const obj4 = {...obj1, ...obj2}
+console.log(obj4);  //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+//if you want to print only keys
+console.log(Object.keys(obj1));  //[ '1', '2' ]
+
+//if you want to print only values
+console.log(Object.values(obj1));  //[ 'a', 'b' ]
