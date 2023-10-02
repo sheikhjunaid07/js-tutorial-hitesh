2b9a862ae920635627a4634a4182bb3299d50368
Sheikh Junaid <88541284+sheikhjunaid07@users.noreply.github.com>
2023-10-02 06:22:14 +0000

    IIFE

---
+++ 03_Functions_and_Scopes/05_IIFE.js
@@ -0,0 +1,22 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+//Normal function
+function one() {
+  console.log("Normal Function!!");
+}
+one(); //Normal Function!!
+//if you made your function IIFE you can simply bound your function inside parenthesis
+(function two() {
+  console.log("IIFE Function");
+})(); //always add ";" after function end than start writing another function
+//First () for function Definition and second () for execution
+
+// IIFE with Arrow Function
+(() => {
+  console.log("IIFE with Arrow Function!!");
+})(); //IIFE with Arrow Function!!
+
+// with parameter
+((name) => {
+  console.log(`Hey! ${name}`);
+})("Junaid"); //Hey! Junaid
